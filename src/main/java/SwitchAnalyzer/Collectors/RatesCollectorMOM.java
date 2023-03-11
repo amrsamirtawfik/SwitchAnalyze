f9e51225d0600088c3e53541ac52c670b867ef0e
ZiadFahmy <68162606+zyadfahmy2@users.noreply.github.com>
2023-03-11 20:12:52 +0200

    added collectors of MOM

--- src/main/java/SwitchAnalyzer/Collectors/RatesCollectorMOM.java
+++ src/main/java/SwitchAnalyzer/Collectors/RatesCollectorMOM.java
@@ -1,37 +1,38 @@
-//package SwitchAnalyzer.Collectors;
-//
-//import SwitchAnalyzer.Machines.MasterOfHPC;
-//import SwitchAnalyzer.NamingConventions;
-//
-//import static SwitchAnalyzer.MainHandler_Master.master;
-//
-///**
-// * this class is responsible for collecting the packetLoss from the machines
-// * do some processing and then send return the overall packetLoss so that
-// * the master can send it to the MOM
-// * the collector must have a name so that the master can identify it
-// * note I assumed that the the MasterOfHPC already have the machines in its list
-// */
-//public class RatesCollectorMOM implements Collector {
-//    public static MasterOfHPC myHPC ;
-//    //the name of the collector is used to identify the collector in the results map
-//    private String name = "Rates";
-//    public String getName() {
-//        return name;
-//    }
-//    @Override
-//    public String collect()
-//    {
-//            float OverallRate;
-//            // this variable is made because the result from is the map is a string
-//            String overAllRateString;
-//            OverallRate= 0;
-//            for (int i = 0; i < master.childNodes.size(); i++) {
-//                //convert the string to a float
-//                overAllRateString = master.childNodes.get(i).machineInfo.map.get(NamingConventions.rates);
-//                OverallRate += Float.parseFloat(overAllRateString);
-//            }
-//            return String.valueOf(OverallRate);
-//    }
-//
-//}
+package SwitchAnalyzer.Collectors;
+
+import SwitchAnalyzer.Machines.MasterOfHPC;
+import SwitchAnalyzer.NamingConventions;
+
+import static SwitchAnalyzer.MainHandler_MOM.masterOfMasters;
+import static SwitchAnalyzer.MainHandler_Master.master;
+
+/**
+ * this class is responsible for collecting the packetLoss from the machines
+ * do some processing and then send return the overall packetLoss so that
+ * the master can send it to the MOM
+ * the collector must have a name so that the master can identify it
+ * note I assumed that the the MasterOfHPC already have the machines in its list
+ */
+public class RatesCollectorMOM implements Collector {
+    public static MasterOfHPC myHPC ;
+    //the name of the collector is used to identify the collector in the results map
+    private String name = "Rates";
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String collect()
+    {
+            float OverallRate;
+            // this variable is made because the result from is the map is a string
+            String overAllRateString;
+            OverallRate= 0;
+            for (int i = 0; i < masterOfMasters.HPCs.size(); i++) {
+                //convert the string to a float
+                overAllRateString = masterOfMasters.HPCs.get(i).hpcInfo.map.get(NamingConventions.rates);
+                OverallRate += Float.parseFloat(overAllRateString);
+            }
+            return String.valueOf(OverallRate);
+    }
+
+}
