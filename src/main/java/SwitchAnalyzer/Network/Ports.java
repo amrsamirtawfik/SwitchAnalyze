ceda3af3faff896720e9b2a6e5087853141f5164
Fady Ibrahim <fadyis2014@gmail.com>
2023-03-11 23:13:45 +0200

    Consume configurations and initialize MasterOfHPC, Nodes , Maps(portnumber to HPC map) and HPC machines arraylist

--- src/main/java/SwitchAnalyzer/Network/Ports.java
+++ src/main/java/SwitchAnalyzer/Network/Ports.java
@@ -2,7 +2,7 @@ package SwitchAnalyzer.Network;
 
 public class Ports {
     public static String port1= "9092";
-    public static int HPCportNumber=9090;
+    public static int HPCportNumber=1;
 
     public  static  String sniffingPort = "777";
     public static int webSocketPort = 1234;
